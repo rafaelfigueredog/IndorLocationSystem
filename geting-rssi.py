5b138f99485a7d3646051988421bd5c0891e93fc
Rafael Guimarães <rafaelfigueredog@gmail.com>
2019-05-25 13:37:45 -0300

    coletando RSSI com timer

--- geting-rssi.py
+++ geting-rssi.py
@@ -1,30 +1,37 @@
 from bluepy.btle import Scanner, DefaultDelegate
 import time
 
+
 class ScanDelegate(DefaultDelegate):
     def __init__(self):
         DefaultDelegate.__init__(self)
 
     def handleDiscovery(self, dev, isNewDev, isNewData):
-        if isNewDev:
-            """ print "Discovered device", dev.addr """
+        """ if isNewDev:
+            print "Discovered device", dev.addr
         elif isNewData:
-            print "Received new data from", dev.addr
+            print "Received new data from", dev.addr """
 
-dist = []
-for i in range(10):
+
+
+
+RSSI = []
+timeout = time.time() + 300  
+while True:
+    if time.time() >= timeout:
+        break
     
     scanner = Scanner().withDelegate(ScanDelegate())
-    devices = scanner.scan(10.0)
-
-    if (len(devices) == 0):
-        dist.append("NR")
-        continue
+    devices = scanner.scan(1.0)
 
     for dev in devices:
-        print "Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi)
-        dist.append(dev.rssi)
-        """ for (adtype, desc, value) in dev.getScanData():
-            print "  %s = %s" % (desc, value) """
+        if (dev.addr == "fb:c6:58:b7:fd:e1"):
+            print '\x1b[2J\x1b[1;1H'
+            print 'RSSI:', dev.rssi, 'dbm' 
+            RSSI.append(dev.rssi)
+        #print "Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi)
+        #for (adtype, desc, value) in dev.getScanData():
+        #    print "  %s = %s" % (desc, value)
 
-print(dist)
+print(RSSI)
+print(len(RSSI))
